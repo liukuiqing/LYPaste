3801fa5176435aad45f4774e238c5e74d2ced587
liukuiqing <liukuiqing@diyiedu.com>
2022-04-07 17:52:40 +0800

    创建项目

---
+++ LYPaste/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LYPaste
+//
+//  Created by dy_liukuiqing on 2022/4/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LYPaste/LYPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LYPasteApp.swift
+//  LYPaste
+//
+//  Created by dy_liukuiqing on 2022/4/7.
+//
+
+import SwiftUI
+
+@main
+struct LYPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
