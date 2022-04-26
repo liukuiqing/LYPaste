1abe14f5e1a97921d026c0654188b98a4f6fe75c
liukuiqing <18155167935@163.com>
2022-04-26 10:53:41 +0800

    LYPasteSwift

---
+++ LYPasteSwift/LYPasteSwift/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  LYPasteSwift
+//
+//  Created by dy_liukuiqing on 2022/4/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
