971e6a22464b60b95b252405ed793dabe6b97b56
yangtianyan <yangtianyan@diyiedu.com>
2022-06-16 09:18:26 +0800

    feat: 释放listview

--- LYPasteSwift/LYPasteSwift/base/Util/LYPasterShowManager.swift
+++ LYPasteSwift/LYPasteSwift/base/Util/LYPasterShowManager.swift
@@ -11,13 +11,15 @@ class LYPasterShowManager {
     static let instance = LYPasterShowManager()
     private var eventMonitor: EventMonitor?
     private var window: NSWindow?
+    private var windowController: NSWindowController?
+    private var isHidden = true
     
     init() {
         eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown], handler: mouseEventHandler)
     }
     
     @objc public func toggleWindow() {
-        if let _ = window {
+        if  !isHidden {
             hideWindow()
         }
         else {
@@ -33,31 +35,32 @@ class LYPasterShowManager {
     func showPasteVC() {
         var rect = NSScreen.main?.frame ?? NSMakeRect(0, 0, 640, 480)
         rect = NSRect(x: 0, y: 0, width: rect.width, height: 350)
-        let window = LYPasterWindow.init(contentRect: rect, styleMask: [], backing: .buffered, defer: true, screen: NSScreen.main)
-        window.backgroundColor = .windowBackgroundColor
-        window.level = NSWindow.Level.popUpMenu
-        window.isMovableByWindowBackground = false
-        
+        if self.window == nil {
+            let window = LYPasterWindow.init(contentRect: rect, styleMask: [], backing: .buffered, defer: true, screen: NSScreen.main)
+            window.backgroundColor = .windowBackgroundColor
+            window.level = NSWindow.Level.popUpMenu
+            window.isMovableByWindowBackground = false
+            self.window = window
+            let wc: NSWindowController = NSWindowController.init(window: window)
+            wc.loadWindow()
+        }
         /// todo : 此处将显示的view赋值给contentView
         let listview =  LYPasteListView.init(frame: NSRect.init(x: 0, y: 18, width: rect.width, height: 295))
-        window.contentView = listview
         listview.voidBlock()
         LYPasterData.instance.createTable(table: TestTableModel.tabName, of: TestTableModel.self)
         LYPasterMonitor.shareInstance().newPateFunc = listview
-        self.window = window
-        let wc: NSWindowController = NSWindowController.init(window: window)
-        wc.window?.contentView = listview
-        wc.window?.makeKeyAndOrderFront(window)
-        wc.showWindow(listview)
-        wc.loadWindow()
+        self.window?.contentView = listview
+        self.window?.makeKeyAndOrderFront(window)
+        isHidden = false
     }
     
     public func hideWindow() {
         eventMonitor?.stop()
         self.window?.orderOut(self.window)
+        self.window?.contentView = nil
         NSApplication.shared.stopModal()
-        self.window = nil
         LYPasterMonitor.shareInstance().searhKey = nil
+        isHidden = true
     }
     
     func mouseEventHandler(_ event: NSEvent?) {
