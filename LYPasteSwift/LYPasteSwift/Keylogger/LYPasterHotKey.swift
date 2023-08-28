c0cc5f5ea8e3be8d01ce0a4cd5a1250b1ec51609
liukuiqing <18155167935@163.com>
2023-08-28 18:46:30 +0800

    注释esc代码，以后再优化

--- LYPasteSwift/LYPasteSwift/Keylogger/LYPasterHotKey.swift
+++ LYPasteSwift/LYPasteSwift/Keylogger/LYPasterHotKey.swift
@@ -25,12 +25,83 @@ class LYPasterHotKey {
         let _ = RegisterEventHotKey(UInt32(Keycode.escape),
                                     UInt32(kNullCharCode),
                                     escHotKeyId,
-                                    GetEventDispatcherTarget(),
+                                    GetApplicationEventTarget(),
                                     0,
                                     &carbonHotKey)
         installHotKeyPressedEventHandler()
+        /*
+//        let escape = NSEvent.EventTypeMask(rawValue: UInt64(Keycode.escape))
+        let escape = NSEvent.EventTypeMask.flagsChanged
+        NSEvent.addLocalMonitorForEvents(matching: escape) { event in
+            print("modifierFlags==> \(event.modifierFlags.rawValue) ")
+            return event
+        }
+        NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { event in
+            print("keyUp3242==> \(event.modifierFlags.rawValue) ")
+//            return event
+        }
+//        UIApplicationDidReceiveKeyDownNotification
+//        NSUserNotificationCenter.default.addObserver(<#T##observer: NSObject##NSObject#>, forKeyPath: <#T##String#>, context: <#T##UnsafeMutableRawPointer?#>)
+         */
     }
-    
+//    模拟键盘事件
+    class func postKeyboardEvent(_ virtualKey:CGKeyCode, keyDown:Bool,flags:CGEventFlags) {
+        let source = CGEventSource(stateID: .privateState)
+        let push = CGEvent(keyboardEventSource: source, virtualKey: virtualKey, keyDown: keyDown)
+//        push?.flags = flags
+//        CGEvent.tapPostEvent(push!)
+//        let source = CGEventSourceCreate(.privateState)
+//        let push = CGEventCreateKeyboardEvent(source, virtualKey, keyDown)
+//        push.post(tap: kCGHIDEventTap)
+        push?.post(tap: .cghidEventTap)
+        
+//        CGEventSetFlags(push, flags);
+//        CGEventPost(kCGHIDEventTap, push);
+//        CFRelease(push);
+//        CFRelease(source);
+        // 模拟按下键盘按键
+               let keyDownEvent = NSEvent.keyEvent(
+                   with: .keyDown,
+                   location: NSPoint(x: 0, y: 0),
+                   modifierFlags: [],
+                   timestamp: 0,
+                   windowNumber: 0,
+                   context: nil,
+                   characters: "",
+                   charactersIgnoringModifiers: "",
+                   isARepeat: false,
+                   keyCode: virtualKey
+               )
+               NSApplication.shared.postEvent(keyDownEvent!, atStart: true)
+               
+        
+        // 获取其他应用程序的输入框元素
+//                let inputField = AXUIElementCreateApplication(
+//                    ProcessSerialNumber(highLongOfPSN: 0, lowLongOfPSN: UInt32(kCurrentProcess)),
+//                    pid_t(YourAppProcessID)
+//                ).getElement()
+        
+        var appBundleIdentifier = "com.example.targetapp" // 目标应用程序的 Bundle Identifier
+        let workspace = NSWorkspace.shared
+        if let frontApp = workspace.frontmostApplication {
+            print("Bundle Identifier: \(frontApp.bundleIdentifier ?? "")")
+            print("LocalizedName: \(frontApp.localizedName ?? "")")
+            appBundleIdentifier = frontApp.bundleIdentifier ?? ""
+        }
+        
+        // 获取其他应用程序的输入框元素
+        let targetApp = NSRunningApplication.runningApplications(withBundleIdentifier: appBundleIdentifier).first
+        // 模拟粘贴操作
+        if ((targetApp?.activate(options: .activateAllWindows)) != nil) {
+            // 使用辅助功能 API 模拟键盘操作
+            let pasteEvent = NSAppleScript(source: """
+                        tell application "System Events"
+                        keystroke "v" using {command down}
+                        end tell
+                        """)
+            pasteEvent?.executeAndReturnError(nil)
+        }
+    }
     class func installHotKeyPressedEventHandler() {
         var pressedEventType = EventTypeSpec()
         pressedEventType.eventClass = OSType(kEventClassKeyboard)
@@ -39,6 +110,7 @@ class LYPasterHotKey {
         InstallEventHandler(GetEventDispatcherTarget(), { _, inEvent, _ -> OSStatus in
             return LYPasterHotKey.sendPressedKeyboardEvent(inEvent!)
         }, 1, &pressedEventType, nil, nil)
+        
     }
 
     class func sendPressedKeyboardEvent(_ event: EventRef) -> OSStatus {
@@ -59,10 +131,12 @@ class LYPasterHotKey {
         case EventParamName(kEventHotKeyPressed):
             if hotKeyId.id == 1{
                 LYPasterShowManager.instance.hideWindow()
+//                postKeyboardEvent( Keycode.v | Keycode.command, keyDown: true, flags: CGEventFlags.init(rawValue: 0))
             }else{
                 LYPasterShowManager.instance.toggleWindow()
             }
             break
+            
         default:
             assert(false, "Unknown event kind")
         }
